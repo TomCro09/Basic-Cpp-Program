10949f79bfaa9af8f617a3ee97ab5e0b133178b8
Tom Cronin <TJCcronin@protonmail.com>
2024-02-27 15:28:28 +0000

    Add files via upload

---
+++ firstc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This is a comment on a single line
+    
+    /* This is a 
+    multi line comment*/
+    cout << "Hello World! \n";
+    cout << "This is a basic C++ setup";
+    return 0;
+
+}
